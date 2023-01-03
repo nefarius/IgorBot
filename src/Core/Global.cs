31c77133395c5f28e8bebb9f87911a65f742df87
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-01-03 03:56:04 +0100

    Added comments

--- src/Core/Global.cs
+++ src/Core/Global.cs
@@ -6,9 +6,11 @@ namespace IgorBot.Core;
 
 /// <summary>
 ///     This is a terrible pattern but it simplifies certain things...
+///     TODO: unused, remove?
 /// </summary>
 [SuppressMessage("ReSharper", "UnusedMember.Global")]
 [SuppressMessage("ReSharper", "PrivateFieldCanBeConvertedToLocalVariable")]
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
 internal class Global
 {
     public Global(IServiceProvider serviceProvider)
