ef6ecc14d42910f5535c0b0a64f64e8d0320c49d
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-01-03 03:34:37 +0100

    Refined logging

--- src/Schema/GuildMember.PropertyChanged.cs
+++ src/Schema/GuildMember.PropertyChanged.cs
@@ -12,7 +12,8 @@ internal sealed partial class GuildMember
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public void OnPropertyChanged(string propertyName, object before, object after)
     {
-        Log.Debug("{Property} changed", propertyName);
+        Log.Debug("{Property} changed from {Before} to {After}",
+            propertyName, before, after);
 
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
