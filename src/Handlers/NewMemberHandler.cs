08db1ddcb0786c609ebbb9affc9fc72374369754
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-02-15 18:39:09 +0100

    Improved error handling

--- src/Handlers/NewMemberHandler.cs
+++ src/Handlers/NewMemberHandler.cs
@@ -2,6 +2,7 @@
 
 using DSharpPlus;
 using DSharpPlus.Entities;
+using DSharpPlus.Exceptions;
 
 using IgorBot.Core;
 using IgorBot.Schema;
@@ -43,9 +44,9 @@ internal sealed class NewMemberHandler : IHandleMessages<NewMemberMessage>
         _logger.LogInformation("Processing new member workflow");
 
         GuildMember dbMember = await DB.Find<GuildMember>().OneAsync(message.MemberEntiryId);
-        
+
         _logger.LogDebug("Got member from DB: {@Member}", dbMember);
-        
+
         if (dbMember.Channel is not null || dbMember.IsOnboardingInProgress)
         {
             _logger.LogWarning("Member {Member} already has an active newbie channel, aborting", dbMember);
@@ -85,25 +86,25 @@ internal sealed class NewMemberHandler : IHandleMessages<NewMemberMessage>
             // 
             foreach (ulong moderatorRoleId in guildConfig.ApplicationModeratorRoleIds)
             {
-                DiscordRole role = guild.GetRole(moderatorRoleId);
+                try
+                {
+                    DiscordRole role = guild.GetRole(moderatorRoleId);
+                    DiscordOverwriteBuilder overwrite = new(role);
 
-                if (role is null)
+                    overwrite.Allow(Permissions.AccessChannels);
+                    overwrite.Allow(Permissions.ReadMessageHistory);
+                    overwrite.Allow(Permissions.ManageMessages);
+                    overwrite.Allow(Permissions.SendMessages);
+                    overwrite.Allow(Permissions.EmbedLinks);
+                    overwrite.Allow(Permissions.AddReactions);
+
+                    overwrites.Add(overwrite);
+                }
+                catch (ServerErrorException)
                 {
                     _logger.LogWarning("Role with ID {Id} wasn't found in the Discord universe, skipping",
                         moderatorRoleId);
-                    continue;
                 }
-
-                DiscordOverwriteBuilder overwrite = new(role);
-
-                overwrite.Allow(Permissions.AccessChannels);
-                overwrite.Allow(Permissions.ReadMessageHistory);
-                overwrite.Allow(Permissions.ManageMessages);
-                overwrite.Allow(Permissions.SendMessages);
-                overwrite.Allow(Permissions.EmbedLinks);
-                overwrite.Allow(Permissions.AddReactions);
-
-                overwrites.Add(overwrite);
             }
 
             //
