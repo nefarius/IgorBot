f72f9cf48aa45b66d67f75b6b326dc70737ade87
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-05-30 20:02:34 +0200

    Update HoneypotModule.cs

--- src/Modules/HoneypotModule.cs
+++ src/Modules/HoneypotModule.cs
@@ -26,10 +26,15 @@ internal sealed class HoneypotModule(IOptionsMonitor<IgorConfig> config, ILogger
     {
         try
         {
+            if (args.Author.IsBot)
+            {
+                return;
+            }
+
             DiscordMember member = await args.Guild.GetMemberAsync(args.Author.Id);
 
             // do not apply to privileged accounts
-            if (args.Author.IsBot || member.IsOwner)
+            if (member.IsOwner)
             {
                 return;
             }
