7d9c48ca7dd6aec3b793e376eeab6d8911f1d428
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-05-31 15:32:14 +0200

    Update MemberDbSyncInvokable.cs

--- src/Invocables/MemberDbSyncInvokable.cs
+++ src/Invocables/MemberDbSyncInvokable.cs
@@ -24,47 +24,65 @@ internal class MemberDbSyncInvokable(
     {
         logger.LogInformation("Running members database synchronization");
 
-        foreach (GuildConfig guildConfig in config.CurrentValue.Guilds.Select(gc => gc.Value))
+        try
         {
-            DiscordGuild guild = discord.Client.Guilds[guildConfig.GuildId];
-
-            logger.LogDebug("Processing members of {Guild}", guild);
-
-            IReadOnlyCollection<DiscordMember> members = await guild.GetAllMembersAsync();
-
-            foreach (DiscordMember member in members.Where(m => !m.IsBot))
+            foreach (GuildConfig guildConfig in config.CurrentValue.Guilds.Select(gc => gc.Value))
             {
-                string id = member.ToEntityId();
-                GuildMember guildMember = await DB.Find<GuildMember>().OneAsync(id);
+                DiscordGuild guild = discord.Client.Guilds[guildConfig.GuildId];
 
-                // already exists
-                if (guildMember is not null)
+                logger.LogDebug("Processing members of {Guild}", guild);
+
+                IReadOnlyCollection<DiscordMember> members = await guild.GetAllMembersAsync();
+
+                foreach (DiscordMember member in members.Where(m => !m.IsBot))
                 {
-                    // newbie channel object exists in DB but no longer in guild
-                    if (guildMember.Channel is not null && !guild.Channels.ContainsKey(guildMember.Channel.ChannelId))
+                    string id = member.ToEntityId();
+                    GuildMember guildMember = await DB.Find<GuildMember>().OneAsync(id);
+
+                    // already exists
+                    if (guildMember is not null)
                     {
-                        logger.LogInformation("Removing orphaned channel entity {Channel}", guildMember.Channel);
-                        await guildMember.DeleteChannel();
+                        // newbie channel object exists in DB but no longer in guild
+                        if (guildMember.Channel is not null &&
+                            !guild.Channels.ContainsKey(guildMember.Channel.ChannelId))
+                        {
+                            logger.LogInformation("Removing orphaned channel entity {Channel}", guildMember.Channel);
+                            await guildMember.DeleteChannel();
+                        }
+
+                        string currentMemberString = member.ToString();
+
+                        if (!string.IsNullOrEmpty(currentMemberString) && guildMember.Member != currentMemberString)
+                        {
+                            logger.LogInformation("Updating Member property from {Old} to {New} for entity {Member}",
+                                guildMember.Member, currentMemberString, guildMember);
+                            guildMember.Member = currentMemberString;
+                            await guildMember.SaveAsync();
+                        }
+
+                        continue;
                     }
 
-                    continue;
+                    // add missing
+                    guildMember = new GuildMember
+                    {
+                        GuildId = member.Guild.Id,
+                        MemberId = member.Id,
+                        Member = member.ToString(),
+                        Mention = member.Mention
+                    };
+
+                    await guildMember.SaveAsync();
+
+                    logger.LogInformation("{Member} added to DB", member);
                 }
-
-                // add missing
-                guildMember = new GuildMember
-                {
-                    GuildId = member.Guild.Id,
-                    MemberId = member.Id,
-                    Member = member.ToString(),
-                    Mention = member.Mention
-                };
-
-                await guildMember.SaveAsync();
-
-                logger.LogInformation("{Member} added to DB", member);
             }
-        }
 
-        logger.LogInformation("Members database synchronization done");
+            logger.LogInformation("Members database synchronization done");
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occured during sync run");
+        }
     }
 }
